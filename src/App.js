d39f6baea1cfbf07664b7a51d2ffdaefbc3e493c
Tomomi Suda <tomomisuda@Tomomi-no-MacBook-Air.local>
2020-07-17 00:42:03 +0200

    initial commit component without test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './component/header/Header';
+import './app.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
---
+++ src/component/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<header>
+			<div className="wrap">
+				<div className="app-name">
+					TestApp
+				</div>
+			</div>
+		</header>
+	)
+}
+
+export default Header
