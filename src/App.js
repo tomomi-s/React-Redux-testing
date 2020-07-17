9487225d5e573d5a368942d263a346edb3aa4171
Tomomi Suda <tomomisuda@Tomomi-no-MacBook-Air.local>
2020-07-17 18:01:43 +0200

    test headline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './component/header/Header';
+import Headline from './component/headline/Headline';
 import './app.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <section className="main">
+      	<Headline header="Posts" desc="Click the button to render the posts!"/>
+      </section>
     </div>
   );
 }
